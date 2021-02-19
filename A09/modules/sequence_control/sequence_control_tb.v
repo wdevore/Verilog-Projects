ca29478d9ce80dcebbeb4f43d5902b6aa1667d8a
wdevore <william.quartz@gmail.com>
2021-02-19 10:41:00 -0500

    added RegisterFile module

--- A09/modules/sequence_control/sequence_control_tb.v
+++ A09/modules/sequence_control/sequence_control_tb.v
@@ -1,33 +1,44 @@
+`default_nettype none
+
 // --------------------------------------------------------------------------
 // Test bench
 // --------------------------------------------------------------------------
 `timescale 1ns/1ps
 
-module register_tb;
+// Test the sequence control matrix
+
+module register_file_tb;
    parameter Data_WIDTH = 16;                 // data width
+   parameter SelectSize = 3;
    
    // Test bench Signals
    // Outputs
-   wire [Data_WIDTH-1:0] DOut_TB;
+   wire [Data_WIDTH-1:0] SRC1_TB;
+   wire [Data_WIDTH-1:0] SRC2_TB;
 
    // Inputs
-   reg Reset_TB;
-   reg LD_TB;
+   reg REG_WE_TB;
    reg [Data_WIDTH-1:0] DIn_TB;
+   reg [SelectSize-1:0] REG_Dst_TB;
+   reg [SelectSize-1:0] REG_Src1_TB;
+   reg [SelectSize-1:0] REG_Src2_TB;
 
    reg Clock_TB;
 
    // -------------------------------------------
    // Device under test
    // -------------------------------------------
-   Register #(.DataWidth(Data_WIDTH)) dut
+   RegisterFile #(.DataWidth(Data_WIDTH)) dut
    (
-      .Reset(Reset_TB),
       .Clk(Clock_TB),
-      .LD(LD_TB),
+      .REG_WE(REG_WE_TB),
       .DIn(DIn_TB),
-      .DOut(DOut_TB)
-      );
+      .REG_Dst(REG_Dst_TB),
+      .REG_Src1(REG_Src1_TB),
+      .REG_Src2(REG_Src2_TB),
+      .SRC1(SRC1_TB),
+      .SRC2(SRC2_TB)
+   );
 
    // -------------------------------------------
    // Test bench clock - not really need for this TB
@@ -45,56 +56,34 @@ module register_tb;
    // Configure starting sim states
    // -------------------------------------------
    initial begin
-      $dumpfile("register_tb.vcd");  // waveforms file needs to be the same name as the tb file.
+      $dumpfile("register_file_tb.vcd");  // waveforms file needs to be the same name as the tb file.
       $dumpvars;  // Save waveforms to vcd file
       
       $display("%d %m: Starting testbench simulation...", $stime);
 
-      Reset_TB = 1'b1;  // Disable reset
+      REG_WE_TB = 1'b1;  // Disable Register write
       DIn_TB = {Data_WIDTH{1'b0}};  // DIn = 0
-      LD_TB = 1'b1;     // Disable load
+      REG_Dst_TB = 3'b0;      // Default to reg 0
+      REG_Src1_TB = 3'b0;     // Default to reg 0
+      REG_Src2_TB = 3'b0;     // Default to reg 0
    end
 
    always begin
-      // ------------------------------------
-      // Reset first
-      // ------------------------------------
       #50; // Pause for a bit
-      Reset_TB = 1'b0;  // Enable reset
-      DIn_TB = {Data_WIDTH{1'b0}};  // DIn can any value
-      LD_TB = 1'b1;     // Disable load
+
+      // ------------------------------------
+      // Load Reg 0 <- 0x00A0
+      // ------------------------------------
+      REG_WE_TB = 1'b0;  // Enable writing
+      DIn_TB = 16'h00A0;  // Write 0x00A0
+      REG_Dst_TB = 3'b0;    // Select Reg 0 as destination
+      REG_Src1_TB = 3'b0;   // Default to reg 0 as Src #1
 
       #100; // Wait for clock edge to pass
       $display("%d <-- Marker", $stime);
 
-      if (DOut_TB !== 16'h0000) begin
-         $display("%d %m: ERROR - (0) PC output incorrect (%h).", $stime, DOut_TB);
-         $finish;
-      end
-
-      // ------------------------------------
-      // Load
-      // ------------------------------------
-      Reset_TB = 1'b1;  // Disable reset
-      DIn_TB = 16'h00A0;  // Set Address to 0x00A0
-      LD_TB = 1'b0;     // Enable load
-
-      #300; // Wait for clock edge
-
-      if (DOut_TB !== 16'h00A0) begin
-         $display("%d %m: ERROR - (1) PC output incorrect (%h).", $stime, DOut_TB);
-         $finish;
-      end
-
-      // ------------------------------------
-      // Reset
-      // ------------------------------------
-      Reset_TB = 1'b0;  // Enable reset
-      LD_TB = 1'b1;     // Disable load
-      #300; // Wait for clock edge
-
-      if (DOut_TB !== 16'h0000) begin
-         $display("%d %m: ERROR - (2) PC output incorrect (%h).", $stime, DOut_TB);
+      if (SRC1_TB != 16'h00A0) begin
+         $display("%d %m: ERROR - (0) Src #1 output incorrect (%h).", $stime, SRC1_TB);
          $finish;
       end
 
