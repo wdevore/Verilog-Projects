cf69aa6d93c664272956b3c1418941c7c71d36ba
wdevore <william.quartz@gmail.com>
2021-04-07 20:45:45 -0400

    added images of each simulation program

--- A09/components/cpu/cpu_tb.v
+++ A09/components/cpu/cpu_tb.v
@@ -78,13 +78,13 @@ module cpu_tb;
       wait(cpu.ControlMatrix.state === cpu.ControlMatrix.S_Ready);
  
       $display("%d <-- CPU ready", $stime);
-  
+   
       // Check memory was loaded
       if (cpu.memory.mem[0] === 16'h0 || cpu.memory.mem[0] === {DataWidth_TB{1'bx}}) begin
          $display("%d %m: ###ERROR### - Memory doesn't appear to be loaded", $time);
          // $finish;
       end
-
+ 
       // ---------------------------------------------------
       // Wait for the beginning of an instruction.
       // ---------------------------------------------------
@@ -92,10 +92,10 @@ module cpu_tb;
       // wait(cpu.ControlMatrix.state === cpu.ControlMatrix.S_FetchPCtoMEM && cpu.ControlMatrix.next_state === cpu.ControlMatrix.S_FetchMEMtoIR);
       // $display("%d <-- Instruction (%d) at", $stime, cycleCnt);
       // cycleCnt++;
-           
+      
       // `include "tests/add_halt.v"
       // `include "tests/sub_halt.v"
-    
+   
       // Wait for Halt to complete. Waiting on a posedge will
       // conflicts with other waits that occur at the same time,
       // so we wait on the neg-edge.
